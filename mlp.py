21de6b2ffe1b55549bd405088e123d3f8d11ffc2
BD4SUR <20069428+bd4sur@users.noreply.github.com>
2023-10-18 23:31:11 +0800

    update

--- mlp.py
+++ mlp.py
@@ -2,44 +2,46 @@
 # ref https://dennybritz.com/posts/wildml/implementing-a-neural-network-from-scratch/
 # BD4SUR 2023.10
 
+import math
+import random
 import numpy as np
-from sklearn import datasets, linear_model
+from sklearn import datasets
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+fig, ax = plt.subplots()
+frames = []
 
 class Config:
-    nn_input_dim = 2  # input layer dimensionality
-    nn_output_dim = 2  # output layer dimensionality
-    # Gradient descent parameters (I picked these by hand)
-    epsilon = 0.01  # learning rate for gradient descent
-    reg_lambda = 0.01  # regularization strength
+    batch_size = 64
+    learning_rate = 0.01
+    reg_lambda = 0.01
 
 def generate_data():
     np.random.seed(0)
-    X, y = datasets.make_moons(500, noise=0.30)
+    X, y = datasets.make_moons(1024, noise=0.2)
     return X, y
 
-def visualize(X, y, model):
-    # plt.scatter(X[:, 0], X[:, 1], s=40, c=y, cmap=plt.cm.Spectral)
-    # plt.show()
-    plot_decision_boundary(lambda x:predict(model,x), X, y)
-    plt.title("Logistic Regression")
-
-def plot_decision_boundary(pred_func, X, y):
-    # Set min and max values and give it some padding
-    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
-    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+def capture_chart(X, y, model, epoch, loss):
+    x_min, x_max = X[:, 0].min() - 0.5, X[:, 0].max() + 0.5
+    y_min, y_max = X[:, 1].min() - 0.5, X[:, 1].max() + 0.5
     h = 0.01
-    # Generate a grid of points with distance h between them
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-    # Predict the function value for the whole gid
-    Z = pred_func(np.c_[xx.ravel(), yy.ravel()])
+    Z = predict(model, np.c_[xx.ravel(), yy.ravel()])
     Z = Z.reshape(xx.shape)
-    # Plot the contour and training examples
-    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
-    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral)
-    plt.show()
 
-def forward(x, W, b):
+    contour = ax.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    scatter = plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral)
+    title_str = "Loss @ epoch %i : %f" % (epoch, loss)
+    title = plt.text(0, 0, title_str, horizontalalignment="left", verticalalignment="bottom", transform=ax.transAxes)
+    frame = []
+    frame.extend(contour.collections)
+    frame.append(scatter)
+    frame.append(title)
+    frames.append(frame)
+
+def forward(x, model):
+    W, b, af = model["W"], model["b"], model["activation_function"]
     num_hidden_layers = len(W)
     a = [0] * (num_hidden_layers + 1)
     z = [0] * (num_hidden_layers + 1)
@@ -48,104 +50,111 @@ def forward(x, W, b):
         Wi = W[i]
         bi = b[i]
         z[i+1] = a[i].dot(Wi) + bi
-        a[i+1] = np.tanh(z[i+1])
+        # 激活函数
+        if af == "tanh":
+            a[i+1] = np.tanh(z[i+1])        # tanh
+        elif af == "ReLU":
+            a[i+1] = np.maximum(z[i+1], 0)  # ReLU
+        else:
+            raise
     # softmax
     exp_scores = np.exp(z[num_hidden_layers])
-    y_predict = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
-    return (y_predict, a)
+    outputs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+    return (outputs, a)
 
-def bp(y_predict, y, W, b, a, num_examples):
+def dReLU(x):
+    x[x<=0] = 0
+    x[x>0] = 1
+    return x
+
+def bp(outputs, y, a, model):
+    W, af = model["W"], model["activation_function"]
     num_hidden_layers = len(W)
-    delta = y_predict
-    delta[range(num_examples), y] -= 1
+    num_y = len(outputs)
+    delta = outputs
+    delta[range(num_y), y] -= 1  # 交叉熵
     dW = [0] * num_hidden_layers
     db = [0] * num_hidden_layers
     for i in range(num_hidden_layers):
         i = num_hidden_layers - i - 1
-        dW[i] = (a[i].T).dot(delta) + Config.reg_lambda * W[i] # L2-regularization
+        dW[i] = (a[i].T).dot(delta) + Config.reg_lambda * W[i] # L2-正则化
         if i == num_hidden_layers - 1:
             db[i] = np.sum(delta, axis=0, keepdims=True)
         else:
             db[i] = np.sum(delta, axis=0)
-        delta = delta.dot(W[i].T) * (1 - np.power(a[i], 2))
+        # 激活函数
+        if af == "tanh":
+            delta = delta.dot(W[i].T) * (1 - np.power(a[i], 2))  # tanh
+        elif af == "ReLU":
+            delta = delta.dot(W[i].T) * dReLU(a[i])              # ReLU
+        else:
+            raise
     return (dW, db)
 
 def calculate_loss(model, X, y):
-    num_examples = len(X)  # training set size
-    W, b = model['W'], model['b']
-    # Forward propagation to calculate our predictions
-    y_predict = forward(X, W, b)[0]
-    # Calculating the loss
-    corect_logprobs = -np.log(y_predict[range(num_examples), y])
-    data_loss = np.sum(corect_logprobs)
-    # Add L2 regulatization term to loss (optional)
+    x_num = len(X)
+    W = model['W']
+    outputs = forward(X, model)[0]
+    data_loss = np.sum(-np.log(outputs[range(x_num), y]))
+    # L2-正则化
     sum = 0
     for i in range(len(W)):
         sum += np.sum(np.square(W[i]))
     data_loss += Config.reg_lambda / 2 * sum
-    return 1. / num_examples * data_loss
+    return 1.0 / x_num * data_loss
 
 def predict(model, x):
-    W, b = model['W'], model['b']
-    # Forward propagation
-    y_predict = forward(x, W, b)[0]
-    return np.argmax(y_predict, axis=1)
+    output = forward(x, model)[0]
+    return np.argmax(output, axis=1)
 
 # layer_dims = [input_layer_dim, hidden1, hidden2, ... , output_layer_dim]
-def build_model(X, y, layer_dims, num_passes=20000, print_loss=False):
-    # Initialize the parameters to random values. We need to learn these.
-    num_examples = len(X)
-
+def init_model(layer_dims, activation_function="tanh"):
     num_hidden_layers = len(layer_dims) - 1
     np.random.seed(0)
-
     W = [0] * num_hidden_layers
     b = [0] * num_hidden_layers
     for layer_index in range(num_hidden_layers):
         W[layer_index] = np.random.randn(layer_dims[layer_index], layer_dims[layer_index + 1]) / np.sqrt(layer_dims[layer_index])
         b[layer_index] = np.zeros((1, layer_dims[layer_index + 1]))
+    return { "W": W, "b": b, "num_hidden_layers": num_hidden_layers, "activation_function": activation_function }
 
-    # This is what we return at the end
-    model = {}
+def train(X, y, model, epoch_num=100):
+    W, b, num_hidden_layers = model["W"], model["b"], model["num_hidden_layers"]
+    training_set_size = len(X)
+    # 完成一个epoch所需的iteration数
+    iteration_num = math.ceil(training_set_size / Config.batch_size)
+    for epoch in range(epoch_num):
+        # 打乱训练集顺序，每个迭代取batch_size个样本进行梯度下降
+        batch_indexes = list(range(training_set_size))
+        random.shuffle(batch_indexes)
+        for iter in range(iteration_num):
+            x_batch = X[batch_indexes[iter * Config.batch_size : (iter+1) * Config.batch_size]]
+            y_batch = y[batch_indexes[iter * Config.batch_size : (iter+1) * Config.batch_size]]
+            # 前向传播
+            outputs, a = forward(x_batch, model)
+            # 反向传播求梯度
+            dW, db = bp(outputs, y_batch, a, model)
+            # 梯度下降优化
+            for layer_index in range(num_hidden_layers):
+                W[layer_index] += -Config.learning_rate * dW[layer_index]
+                b[layer_index] += -Config.learning_rate * db[layer_index]
+            model["W"] = W
+            model["b"] = b
 
-    # Gradient descent. For each batch...
-    for i in range(0, num_passes):
-
-        # Forward propagation
-        y_predict, a = forward(X, W, b)
-
-        # Backpropagation
-        dW, db = bp(y_predict, y, W, b, a, num_examples)
-
-        # Gradient descent parameter update
-        for layer_index in range(num_hidden_layers):
-            W[layer_index] += -Config.epsilon * dW[layer_index]
-            b[layer_index] += -Config.epsilon * db[layer_index]
-
-        # Assign new parameters to the model
-        model = {'W': W, 'b': b}
-
-        # Optionally print the loss.
-        # This is expensive because it uses the whole dataset, so we don't want to do it too often.
-        if print_loss and i % 1000 == 0:
-            print("Loss after iteration %i: %f" % (i, calculate_loss(model, X, y)))
-
+        if epoch % 10 == 0:
+            loss = calculate_loss(model, X, y)
+            print("Loss @ epoch %i: %f" % (epoch, loss))
+            capture_chart(X, y, model, epoch, loss)
     return model
-
-
-def classify(X, y):
-    # clf = linear_model.LogisticRegressionCV()
-    # clf.fit(X, y)
-    # return clf
-
-    pass
-
 
 def main():
     X, y = generate_data()
-    model = build_model(X, y, [Config.nn_input_dim, 5, 4, 3, Config.nn_output_dim], print_loss=True)
-    visualize(X, y, model)
-
+    layer_dims = [2, 5, 5, 5, 2]
+    model = init_model(layer_dims, "tanh")
+    train(X, y, model, 1000)
+    ani = animation.ArtistAnimation(fig, frames, interval=100, blit=True, repeat=True, repeat_delay=0)
+    plt.colorbar()
+    plt.show()
 
 if __name__ == "__main__":
     main()
